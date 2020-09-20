1688c3f5931fd90f84eb005e9e5e12a677ea341e
Maksym Chumak <chumak.maksym@gmail.com>
2020-09-19 18:11:11 -0700

    add initial camera functionality

---
+++ app/src/main/java/com/example/photogallery/CameraActivity.java
@@ -0,0 +1,89 @@
+package com.example.photogallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CameraActivity extends AppCompatActivity {
+    private ImageView img;
+    private File newPhoto;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+         img = (ImageView) findViewById(R.id.img);
+
+    }
+
+    // retrieves the image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("CHECK", Integer.toString(resultCode));
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            img.setImageBitmap(imageBitmap);
+        }
+
+        if (resultCode == RESULT_CANCELED){
+            newPhoto.delete();
+        }
+    }
+
+    // referenced from activity_camera.xml
+    public void takePhoto(View v) throws IOException {
+        dispatchTakePictureIntent();
+    }
+
+    // invoke camera
+    private void dispatchTakePictureIntent() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        newPhoto = createImageFile();
+
+        if (newPhoto != null) {
+
+            // get a URI for the file based on authority of app's own fileprovider
+            Uri photoURI = FileProvider.getUriForFile(this, "com.example.photogallery.fileprovider", newPhoto);
+
+            // takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI); // FIXME: uncomment when saving images. NOTE: sets Bitmap data to null
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+    // create files/Download directory if it does not exist, then creates jpg file for new photo
+    private File createImageFile() throws IOException {
+
+        File downloadFolder = new File(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString());
+
+        // create the Download folder if it does not exist
+        if (!downloadFolder.exists()) {
+            downloadFolder.mkdir();
+        }
+
+        // generate image filename
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        // prepares the final path for the output photo file
+        File image = File.createTempFile(imageFileName,".jpg", downloadFolder);
+
+        return image;
+    }
+}
--- app/src/main/java/com/example/photogallery/MainActivity.java
+++ app/src/main/java/com/example/photogallery/MainActivity.java
@@ -1,41 +1,67 @@
 package com.example.photogallery;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
     public static final int SEARCH_REQUEST = 0;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestStoragePermission();
 
+        // take picture
+        Button snapButton = (Button) findViewById(R.id.btnSnap);
+        snapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                invokeCameraActivity(v);
+            }
+        });
+
+        // open search window
         Button searchButton = (Button) findViewById(R.id.btnSearch);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                openSearchActivity(view);
+            public void onClick(View v) {
+                openSearchActivity(v);
             }
         });
     }
 
+    /* helper methods */
+
+    // request permission to store images
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            // temporary fix to prevent crash if current Android version lower than required SDK
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+            }
+        }
+    }
+
     // search button
-    public void openSearchActivity(View v) {
+    private void openSearchActivity(View v) {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivityForResult(intent, SEARCH_REQUEST);
+    }
+
+    // snap button
+    private void invokeCameraActivity(View v) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 }
