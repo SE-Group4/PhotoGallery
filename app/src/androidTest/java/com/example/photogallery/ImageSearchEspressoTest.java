0c3a3e947d84c7aee01e49f5c8970420e9f72f52
p3dr4m <p3dr4m@hotmail.com>
2020-10-07 21:08:14 -0700

    remove jenkinsfile and add half written test

--- app/src/androidTest/java/com/example/photogallery/ImageSearchEspressoTest.java
+++ app/src/androidTest/java/com/example/photogallery/ImageSearchEspressoTest.java
@@ -1,9 +1,20 @@
 package com.example.photogallery;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.icu.text.SimpleDateFormat;
 
+import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +33,9 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -34,28 +48,43 @@ import static org.hamcrest.Matchers.anything;
  */
 @RunWith(AndroidJUnit4.class)
 public class ImageSearchEspressoTest {
-    @Rule
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
-    @Rule
-    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
+    @Rule public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    @Rule public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    @Rule public IntentsTestRule<MainActivity> intentsRule = new IntentsTestRule<>(MainActivity.class);
     @Test
     public void dateTest() throws InterruptedException {
         String captionText = "caption";
-        String testImageTimestampText = "Timestamp: 2020:09:24 06:24:17";
         String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
-
         onView(withId(R.id.btnSearch)).perform(click());
-        Thread.sleep(500);
-
         onView(withId(R.id.keyword_search_text)).perform(scrollTo(), clearText(), typeText(captionText), closeSoftKeyboard());
         onView(withId(R.id.date_from_text)).perform(clearText(), typeText("2020-08-21"), closeSoftKeyboard());
         onView(withId(R.id.date_to_text)).perform(clearText(), typeText(currentDate), closeSoftKeyboard());
         onView((withId(R.id.btnApplySearch))).perform(click());
-        Thread.sleep(500);
-
         onData(anything()).inAdapterView(withId(R.id.gridView)).atPosition(0).perform(click());
-        Thread.sleep(500);
         onView(withText(captionText)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void cameraTest() {
+        Drawable testPic = activityRule.getActivity().getDrawable(R.drawable.ic_launcher_background);
+//        Bitmap bitmap = Bitmap.createBitmap(testPic.getIntrinsicWidth(), testPic.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = BitmapFactory.decodeResource(
+                InstrumentationRegistry.getTargetContext().getResources(),
+                R.mipmap.ic_launcher);
+        Intent resultData = new Intent();
+        resultData.putExtra("data", bitmap);
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+        // Stub out the Camera. When an intent is sent to the Camera, this tells Espresso to respond
+        // with the ActivityResult we just created
+        intending(toPackage("com.example.photogallery.MainActivity")).respondWith(result);
+
+        // Now that we have the stub in place, click on the button in our app that launches into the Camera
+        onView(withId(R.id.btnSnap)).perform(click());
+
+        // We can also validate that an intent resolving to the "camera" activity has been sent out by our app
+        intended(toPackage("com.android.camera2"));
+    }
+
+
 }
