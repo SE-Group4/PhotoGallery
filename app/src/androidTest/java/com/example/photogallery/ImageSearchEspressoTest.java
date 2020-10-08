2b765e18c6d963f19a8da54705dc105e4723a79f
Maksym Chumak <chumak.maksym@gmail.com>
2020-10-08 06:07:45 -0700

    resolve merge conflicts

--- app/src/androidTest/java/com/example/photogallery/ImageSearchEspressoTest.java
+++ app/src/androidTest/java/com/example/photogallery/ImageSearchEspressoTest.java
@@ -1,6 +1,14 @@
 package com.example.photogallery;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import android.icu.text.SimpleDateFormat;
+
+import androidx.test.InstrumentationRegistry;
 
 import androidx.test.rule.ActivityTestRule;
 
@@ -22,6 +30,9 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -33,54 +44,64 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ImageSearchEspressoTest {
+ @RunWith(AndroidJUnit4.class)
+ public class ImageSearchEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
     @Test
-    public void dateTest() throws InterruptedException {
+    public void dateTest() {
         String captionText = "caption";
         String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         String lat = "49.220509";
         String lng = "-123.007111";
 
         onView(withId(R.id.btnSearch)).perform(click());
-        Thread.sleep(500);
-
         onView(withId(R.id.keyword_search_text)).perform(scrollTo(), clearText(), typeText(captionText), closeSoftKeyboard());
         onView(withId(R.id.date_from_text)).perform(clearText(), typeText("2020-08-21"), closeSoftKeyboard());
         onView(withId(R.id.date_to_text)).perform(clearText(), typeText(currentDate), closeSoftKeyboard());
         onView(withId(R.id.lat_text)).perform(clearText(), typeText(lat), closeSoftKeyboard());
         onView(withId(R.id.lng_text)).perform(clearText(), typeText(lng), closeSoftKeyboard());
         onView((withId(R.id.btnApplySearch))).perform(click());
-        Thread.sleep(500);
-
         onData(anything()).inAdapterView(withId(R.id.gridView)).atPosition(0).perform(click());
-        Thread.sleep(500);
         onView(withText(captionText)).check(matches(isDisplayed()));
     }
 
-     @Test
-     public void locationTest() throws InterruptedException {
-         String wrongLat = "38.220510";
-         String wrongLng = "-122.007110";
-         String captionText = "caption";
+    @Test
+    public void locationTest() throws InterruptedException {
+        String wrongLat = "38.220510";
+        String wrongLng = "-122.007110";
+        String captionText = "caption";
 
-         String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
 
-         onView(withId(R.id.btnSearch)).perform(click());
-         Thread.sleep(500);
-         onView(withId(R.id.keyword_search_text)).perform(scrollTo(), clearText(), typeText(captionText), closeSoftKeyboard());
-         onView(withId(R.id.date_from_text)).perform(clearText(), typeText("2020-08-21"), closeSoftKeyboard());
-         onView(withId(R.id.date_to_text)).perform(clearText(), typeText(currentDate), closeSoftKeyboard());
-         onView(withId(R.id.lat_text)).perform(clearText(), typeText(wrongLat), closeSoftKeyboard());
-         onView(withId(R.id.lng_text)).perform(clearText(), typeText(wrongLng), closeSoftKeyboard());
-         onView((withId(R.id.btnApplySearch))).perform(click());
-         Thread.sleep(500);
+        onView(withId(R.id.btnSearch)).perform(click());
+        onView(withId(R.id.keyword_search_text)).perform(scrollTo(), clearText(), typeText(captionText), closeSoftKeyboard());
+        onView(withId(R.id.date_from_text)).perform(clearText(), typeText("2020-08-21"), closeSoftKeyboard());
+        onView(withId(R.id.date_to_text)).perform(clearText(), typeText(currentDate), closeSoftKeyboard());
+        onView(withId(R.id.lat_text)).perform(clearText(), typeText(wrongLat), closeSoftKeyboard());
+        onView(withId(R.id.lng_text)).perform(clearText(), typeText(wrongLng), closeSoftKeyboard());
+        onView((withId(R.id.btnApplySearch))).perform(click());
 
-         onView(withId(R.id.gridView)).check(matches(hasChildCount(0)));
-     }
+        onView(withId(R.id.gridView)).check(matches(hasChildCount(0)));
+    }
+
+//    // WIP
+//    @Test
+//    public void cameraTest() {
+//        Bitmap bitmap = BitmapFactory.decodeResource(
+//                InstrumentationRegistry.getTargetContext().getResources(),
+//                R.mipmap.ic_launcher);
+//        Intent resultData = new Intent();
+//        resultData.putExtra("data", bitmap);
+//        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+//
+//        intending(toPackage("com.example.photogallery.MainActivity")).respondWith(result);
+//
+//        onView(withId(R.id.btnSnap)).perform(click());
+//
+//        intended(toPackage("com.android.camera2"));
+//    }
+
 }
